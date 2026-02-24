94abeec031796bcc8e2eaae2a1ca14571560b585
Ayalasamson <AS48422722@GMAIL.COM>
2026-02-25 00:13:58 +0200

    feat(step7): DalList singleton, DalConfig and Factory; update DalTest initialization and add dal-config.xml

--- DotNet2025_6810_6335/DalList/Class1.cs
+++ DotNet2025_6810_6335/DalList/Class1.cs
@@ -1,7 +1,7 @@
 ﻿namespace DalList
 {
     // DalList: factory that returns DAL implementations
-    public class DalList : DalApi.IDal
+    internal sealed class DalList : DalApi.IDal
     {
         // כל תכונה מחזירה מופע של מימוש המתאים בתוך פרויקט DalList
         public DalApi.IProduct Product => new Dal.ProductImplementation();
@@ -9,6 +9,15 @@
         public DalApi.ICustomer Customer => new Dal.CustomerImplementation();
 
         public DalApi.ISale Sale => new Dal.SaleImplementation();
+
+        // private ctor for singleton
+        private DalList() { }
+
+        // single instance
+        private static readonly DalList instance = new DalList();
+
+        // public static Instance property for Factory to retrieve
+        public static DalApi.IDal Instance => instance;
     }
 }
 
